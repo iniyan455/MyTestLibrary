74f500d95030021f592e4570835504b8622dead0
iniyan <iniyan455@gmail.com>
2020-05-09 14:46:16 +0530

    test library

---
+++ testlibrary/src/main/java/com/digitap/testlibrary/LogUtils.java
@@ -0,0 +1,11 @@
+package com.digitap.testlibrary;
+
+public class LogUtils {
+
+    public static String name=LogUtils.class.getName();
+
+
+    public static String logmessage(String name){
+        return name;
+    }
+}
